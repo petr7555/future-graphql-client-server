0588d34e8cbc29fd3243cf5aeddd6d9ffde5b683
Petr Janik <485122@mail.muni.cz>
2020-11-29 23:06:23 +0100

    Clear console.logs

--- database/index.js
+++ database/index.js
@@ -5,9 +5,6 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
-console.log(process.env.NODE_ENV);
-console.log(process.env.DATABASE_URL);
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
